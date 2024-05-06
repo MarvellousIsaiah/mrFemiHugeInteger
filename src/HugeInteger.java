6d78c6dcfac3e61b28c970281164457b3b035508
MarvellousIsaiah <isaiahugoeze@gmail.com>
2024-05-06 16:28:08 +0100

    blackHouse

--- src/HugeInteger.java
+++ src/HugeInteger.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class HugeInteger {
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class HugeInteger {
+    private  int [] elementArray = new int[40];
+    public HugeInteger(String elements){
+        int [] newElementArray = new int[elements.length()];
+
+        for(int count = 0; count < elements.length(); count++) {
+            newElementArray[count] = Integer.parseInt(String.valueOf(elements.charAt(count)));
+
+        }
+        this.elementArray = newElementArray;
+
+
+
+    }
+    public HugeInteger(){
+    }
+
+    public int[] getElementArray(){
+        return elementArray;
+    }
+    public int[] parse(String numbers) {
+        int[] digits = new int [numbers.length()];
+        for(int count = 0; count < numbers.length(); count++){
+            digits[count] = Integer.parseInt(String.valueOf(numbers.charAt(count)));
+        }
+        return digits;
+    }
+    public String toString(){
+        return Arrays.toString(this.elementArray);
+    }
+
+
+    public BigInteger add(HugeInteger hugeInteger) {
+             StringBuilder stringBuilder= new StringBuilder();
+             StringBuilder stringBuilder2= new StringBuilder();
+                    for(int index = 0; index < this.elementArray.length; index++){
+                        stringBuilder.append(this.elementArray[index]);
+                    }
+                    BigInteger firstHugeInteger = new BigInteger(stringBuilder+"");
+                  for(int index = 0; index < hugeInteger.getElementArray().length; index++){
+                      stringBuilder2.append(hugeInteger.elementArray[index]);
+                  }
+                   BigInteger secondHugeInteger = new BigInteger(stringBuilder2+"");
+                  return firstHugeInteger.add(secondHugeInteger);
+    }
+
+    public BigInteger subtract(HugeInteger hugeInteger) {
+        StringBuilder stringBuilder= new StringBuilder();
+        StringBuilder stringBuilder2= new StringBuilder();
+        for(int index = 0; index < this.elementArray.length; index++){
+            stringBuilder.append(this.elementArray[index]);
+        }
+        BigInteger firstHugeInteger = new BigInteger(stringBuilder+"");
+        for(int index = 0; index < hugeInteger.getElementArray().length; index++){
+            stringBuilder2.append(hugeInteger.elementArray[index]);
+        }
+        BigInteger secondHugeInteger = new BigInteger(stringBuilder2+"");
+        return firstHugeInteger.subtract(secondHugeInteger);
+
+    }
 }
--- test/HugeIntegerTest.java
+++ test/HugeIntegerTest.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class HugeIntegerTest {
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Target;
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HugeIntegerTest {
+    @Test
+    public void testThatParseMethodCanConvert() {
+        HugeInteger hugeInteger = new HugeInteger();
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] actual = hugeInteger.parse("0123456789012345678901234567890123456789");
+        assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testThatToStringMethodCanConvertToString() {
+        HugeInteger hugeInteger = new HugeInteger("0123456789012345678901234567890123456789");
+        String expected = "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]";
+        String actual = hugeInteger.toString();
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testThatAddMethodCanAdd() {
+        HugeInteger hugeInteger1 = new HugeInteger("0123456789012345678901234567890123456789");
+        HugeInteger hugeInteger = new HugeInteger("0123456789012345678901234567890123456789");
+        BigInteger expected = new BigInteger("0246913578024691357802469135780246913578");
+        BigInteger actual = hugeInteger.add(hugeInteger1);
+        assertEquals(expected, actual);
+
+
+
+    }
+    @Test
+    public void testThatSubtractMethodCanSubtract() {
+        HugeInteger hugeInteger1 = new HugeInteger("0123456789012345678901234567890123456789");
+        HugeInteger hugeInteger = new HugeInteger("0123456789012345678901234567890123456789");
+        BigInteger expected = new BigInteger("0");
+        BigInteger actual = hugeInteger.subtract(hugeInteger1);
+        assertEquals(expected, actual);
+    }
+
 }
